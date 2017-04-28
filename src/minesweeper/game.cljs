4a3a24a7dc4fe033e156f36f5346860d6cae062e
Alex King <alex@alexanderjamesking.com>
2017-04-28 16:19:55 +0100

    Added count of adjacent mines to tiles

--- src/minesweeper/game.cljs
+++ src/minesweeper/game.cljs
@@ -1,11 +1,11 @@
 (ns minesweeper.game)
 
-(defn empty-board []
+(defn empty-board [size]
   (into {} (mapcat
             (fn [k]
               (assoc {} (:key k) k))
-            (for [x (range 9)
-                  y (range 9)]
+            (for [x (range size)
+                  y (range size)]
               {:key (str x "," y)
                :x x
                :y y
@@ -36,6 +36,15 @@
     (map (fn [[x y]] (str x "," y)) coords)))
 
 (defn- count-adjacent-mines [x y board]
-  (let [adjacent-tiles (select-keys board (adjacent-tiles-coordinates x y))
-        adjacent-mines (filter (fn [[k v]] (= (:type v) :mine)) adjacent-tiles)]
-    (count adjacent-mines)))
+  (let [adj-tiles (select-keys board (adjacent-tiles-coordinates x y))
+        adj-mines (filter (fn [[k v]] (= (:type v) :mine)) adj-tiles)]
+    (count adj-mines)))
+
+(defn- label-tiles-with-adjacent-mines [board]
+  (let [tiles-without-mines (filter (fn [[k v]] (not= :mine (:type v))) board)
+        replace-type-with-num-adj-mines (fn [[k v]]
+                                          (let [n (count-adjacent-mines (:x v) (:y v) board)]
+                                            (assoc {} k (assoc v :type (keyword (str n))))))]
+    (->> (mapcat replace-type-with-num-adj-mines tiles-without-mines)
+         (into {})
+         (merge board))))
--- test/minesweeper/game_test.cljs
+++ test/minesweeper/game_test.cljs
@@ -1,12 +1,15 @@
 (ns minesweeper.game-test
   (:require [cljs.test :refer-macros [is testing async]]
             [devcards.core :refer-macros [deftest]]
-            [minesweeper.game :refer [empty-board place-mines count-adjacent-mines]]))
+            [minesweeper.game :refer [empty-board
+                                      place-mines
+                                      count-adjacent-mines
+                                      label-tiles-with-adjacent-mines]]))
 
 (deftest create-empty-board
   (testing "Initialise an empty board with all tiles unrevealed"
-    (let [board (empty-board)]
-      (is (= 81 (count board)))
+    (let [board (empty-board 3)]
+      (is (= 9 (count board)))
       (is (= {:key "0,0"
               :x 0
               :y 0
@@ -15,9 +18,9 @@
 
 (deftest place-mines-on-board
   (testing "randomly placing 5 mines onto a board"
-    (let [board (-> (empty-board) (place-mines 5))
+    (let [board (-> (empty-board 3) (place-mines 5))
           tiles-with-mines (filter (fn [[k v]] (= (:type v) :mine)) board)]
-      (is (= 81 (count board)))
+      (is (= 9 (count board)))
       (is (= 5 (count tiles-with-mines))))))
 
 (defn place-mine-at [x y board]
@@ -25,7 +28,38 @@
 
 (deftest count-adjacent-mines-test
   (testing "mine placed at 3,3"
-    (let [board (->> (empty-board) (place-mine-at 3 3))]
+    (let [board (->> (empty-board 3) (place-mine-at 3 3))]
       (is (= 1 (count-adjacent-mines 3 4 board)))
       (is (= 0 (count-adjacent-mines 3 3 board)))
       (is (= 0 (count-adjacent-mines 0 0 board))))))
+
+(deftest label-tiles-with-number-of-adjacent-mines-2x2
+  (testing "write the number of adjacent mines"
+    (let [board (->> (empty-board 2) (place-mine-at 0 0) (label-tiles-with-adjacent-mines))
+          tile-type (fn [k] (:type (get board k)))]
+      (is (= :mine (tile-type "0,0")))
+      (is (= :1 (tile-type "0,1")))
+      (is (= :1 (tile-type "1,0")))
+      (is (= :1 (tile-type "1,1")))
+      (is (= 4 (count board))))))
+
+(deftest label-tiles-with-number-of-adjacent-mines-3x3
+  (let [board (->> (empty-board 3) (place-mine-at 1 0) (label-tiles-with-adjacent-mines))
+        tile-type (fn [k] (:type (get board k)))]
+
+    (testing "top row"
+      (is (= :1 (tile-type "0,0")))
+      (is (= :mine (tile-type "1,0")))
+      (is (= :1 (tile-type "2,0"))))
+
+    (testing "middle row"
+      (is (= :1 (tile-type "0,1")))
+      (is (= :1 (tile-type "1,1")))
+      (is (= :1 (tile-type "2,1"))))
+
+    (testing "bottom row"
+      (is (= :0 (tile-type "0,2")))
+      (is (= :0 (tile-type "1,2")))
+      (is (= :0 (tile-type "2,2"))))
+
+    (is (= 9 (count board)))))
