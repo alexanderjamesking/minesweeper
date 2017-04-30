73a2103d0bd35701d639586bbbb5f5ac7dc38ef4
Alex King <alex@alexanderjamesking.com>
2017-04-30 10:47:47 +0100

    Added tests

--- test/minesweeper/game_test.cljs
+++ test/minesweeper/game_test.cljs
@@ -20,7 +20,7 @@
               :type nil} (get board "0,0"))))))
 
 (deftest place-mines-on-board
-  (testing "randomly placing 5 mines onto a board"
+  (testing "Randomly placing 5 mines onto a board"
     (let [board (-> (empty-board 3) (place-mines 5))
           tiles-with-mines (filter (fn [[k v]] (= (:type v) :mine)) board)]
       (is (= 9 (count board)))
@@ -30,20 +30,23 @@
   (assoc-in board [(str x "," y) :type] :mine))
 
 (deftest count-adjacent-mines-test
-  (testing "mine placed at 3,3"
+  (testing "Mine placed at 3,3"
     (let [board (->> (empty-board 3) (place-mine-at 3 3))]
       (is (= 1 (count-adjacent-mines 3 4 board)))
       (is (= 0 (count-adjacent-mines 3 3 board)))
       (is (= 0 (count-adjacent-mines 0 0 board))))))
 
 (deftest one-adjacent-mine
-  (let [board (->> (empty-board 2) (place-mine-at 0 0) (label-tiles-with-adjacent-mines))
-        tile-type (fn [k] (:type (get board k)))]
-    (is (= :mine (tile-type "0,0")))
-    (is (= :1 (tile-type "0,1")))
-    (is (= :1 (tile-type "1,0")))
-    (is (= :1 (tile-type "1,1")))
-    (is (= 4 (count board)))))
+  (testing "Tile type is :1 when there is 1 adjacent mine"
+    (let [board (->> (empty-board 2)
+                     (place-mine-at 0 0)
+                     (label-tiles-with-adjacent-mines))
+          tile-type (fn [k] (:type (get board k)))]
+      (is (= :mine (tile-type "0,0")))
+      (is (= :1 (tile-type "0,1")))
+      (is (= :1 (tile-type "1,0")))
+      (is (= :1 (tile-type "1,1")))
+      (is (= 4 (count board))))))
 
 (deftest two-adjacent-mines
   (let [board (->> (empty-board 3)
@@ -92,7 +95,33 @@
       (view/render-board board-revealed)])))
 
 
-(defcard board-with-three-mines-reveal-empty-space
+
+(deftest board-with-single-mine-reveal-empty-space-test
+  (testing "Clicking on an empty square reveals all adjacent empty squares"
+    (let [board (->> (empty-board 3)
+                     (place-mine-at 0 0)
+                     (label-tiles-with-adjacent-mines))
+          board-revealed (game/reveal-adjacent-empty-tiles board "2,2")
+          tile-state (fn [k] (:state (get board-revealed k)))
+
+
+          ]
+
+      (testing "Mine at 0,0 is unrevealed"
+        (is (= :unrevealed (tile-state "0,0"))))
+
+      (testing "All other tiles revealed"
+        (is (= :revealed (tile-state "1,0")))
+        (is (= :revealed (tile-state "2,0")))
+        (is (= :revealed (tile-state "0,1")))
+        (is (= :revealed (tile-state "1,1")))
+        (is (= :revealed (tile-state "2,1")))
+        (is (= :revealed (tile-state "0,2")))
+        (is (= :revealed (tile-state "1,2")))
+        (is (= :revealed (tile-state "2,2")))))))
+
+
+(defcard board-with-two-mines-reveal-empty-space
   (let [board (->> (empty-board 5)
                    (place-mine-at 0 0)
                    (place-mine-at 3 4)
@@ -107,18 +136,45 @@
       [:p "After clicking the last tile on the right on the first row:"]
       (view/render-board board-revealed)])))
 
-(deftest board-with-three-mines-reveal-empty-space-test
-  (let [board (->> (empty-board 5)
-                   (place-mine-at 0 0)
-                   (place-mine-at 3 4)
-                   (label-tiles-with-adjacent-mines))
-        board-revealed (game/reveal-adjacent-empty-tiles board "4,0")
-        tile-state (fn [k] (:state (get board-revealed k)))]
+(defn- select-tile-keys [board state]
+  (-> (filter (fn [[k v]] (= state (:state v))) board)
+      keys
+      sort))
 
-    (is (= :unrevealed (tile-state "0,0")))
-    (is (= :revealed (tile-state "0,1")))
-    (is (= :revealed (tile-state "0,2")))
-    (is (= :revealed (tile-state "0,3")))
-    (is (= :revealed (tile-state "0,4")))
-    (is (= :unrevealed (tile-state "3,4")))
-    (is (= :unrevealed (tile-state "4,4")))))
+(deftest board-with-two-mines-reveal-empty-space-test
+  (testing "Clicking on an empty square reveals all adjacent empty squares"
+    (let [board (->> (empty-board 5)
+                     (place-mine-at 0 0)
+                     (place-mine-at 3 4)
+                     (label-tiles-with-adjacent-mines))
+          board-revealed (game/reveal-adjacent-empty-tiles board "4,0")
+          tile-state (fn [k] (:state (get board-revealed k)))
+          revealed-keys (select-tile-keys board-revealed :revealed)]
+
+      (testing "The two mines are unrevealed"
+        (is (= :unrevealed (tile-state "0,0")))
+        (is (= :unrevealed (tile-state "3,4"))))
+
+      (testing "The bottom right tile remains unrevealed"
+        (is (= :unrevealed (tile-state "4,4"))))
+
+      (testing "All other tiles are revealed"
+        (is (= ["0,1" "0,2" "0,3" "0,4" "1,0" "1,1" "1,2" "1,3" "1,4"
+                "2,0" "2,1" "2,2" "2,3" "2,4" "3,0" "3,1" "3,2" "3,3"
+                "4,0" "4,1" "4,2" "4,3"]
+               revealed-keys))))))
+
+(deftest reveal-adjacent-tiles-only-reveals-single-tile
+  (testing "Clicking on a tile with one surrounding mine reveals only that tile"
+    (let [board (->> (empty-board 3)
+                     (place-mine-at 0 0)
+                     (label-tiles-with-adjacent-mines))
+          board-revealed (game/reveal-adjacent-empty-tiles board "1,1")
+          tile-state (fn [k] (:state (get board-revealed k)))
+          unrevealed-keys (select-tile-keys board-revealed :unrevealed)]
+
+      (testing "Tile at 1,1 is revealed"
+        (is (= :revealed (tile-state "1,1"))))
+
+      (testing "All other tiles are unrevealed"
+        (is (= ["0,0" "0,1" "0,2" "1,0" "1,2" "2,0" "2,1" "2,2"] unrevealed-keys))))))
